12ed3d2882c7930e70da414ed3682dfc9998f421
3A932010 <jack37699646@gmail.com>
2022-11-04 21:45:21 +0800

    Route 對應 Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,14 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home.index');
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
-Route::get('/about', function () {
-    return view('welcome');
-})->name('about.index');
+Route::get('/about', [AboutController::class, 'index'])->name('about.index');
 
-Route::get('/news', function () {
-    return view('welcome');
-})->name('news.index');
+Route::get('/news', [NewsController::class, 'index'])->name('news.index');
